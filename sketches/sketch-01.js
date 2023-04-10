69beae6ee824f0fc8b48ca72843ea8e37c1cdfa0
Emmanuel527 <arowoseluemmanuel@gmail.com>
2023-04-10 13:00:53 -0500

    🎨

---
+++ sketches/sketch-01.js
@@ -0,0 +1,37 @@
+const canvasSketch = require('canvas-sketch');
+
+const settings = {
+  dimensions: [ 600, 600 ]
+};
+
+const sketch = () => {
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white';
+    context.fillRect(0, 0, width, height);
+    
+    const w = 60;
+    const h = 60;
+    const gap = 20;
+    let x,y
+    
+    for ( let i = 0; i < 5; i++) {
+      
+      for ( let j = 0; j < 5; j++) {
+         x = 100 + (width + gap) * i;
+         y = 100 + (height + gap) *j;
+         
+         context.beginPath();
+         context.rect( x, y, width, height );
+         context.stroke();
+         
+         if ( Math.random() > 0.5 ) {
+          context.beginPath();
+          context.rect( x + 8, y + 8, width - 16, height - 16 );
+          context.stroke();
+        }
+      }
+    }
+  };
+};
+
+canvasSketch(sketch, settings);
